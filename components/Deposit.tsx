09a49a344669cc7871cd819329bf265f5f588878
YuchaoZhang1229 <ec22368@bristol.ac.uk>
2023-04-23 04:06:37 +0100

    fifth commit

--- components/Deposit.tsx
+++ components/Deposit.tsx
@@ -34,7 +34,7 @@ const Deposit: React.FC<Props> = () => {
 
                     <div className="flex flex-row gap-3 mx-auto">
                         <ApproveButton tokenAddress="0x8a0E31de20651fe58A369fD6f76c21A8FF7f8d42" tokenAmount={value} />
-                        <Button onClick={() => supplyLink()} gradientMonochrome="info" className="w-24"> Supply</Button>
+                        <Button  gradientMonochrome="info" className="w-24"> Supply</Button>
                     </div>
 
                     <div className="text-base font-bold">
@@ -45,7 +45,7 @@ const Deposit: React.FC<Props> = () => {
                         type="text"
                         sizing="md"
                         className="w-full" />
-                    <Button onClick={() => withdrawLink()} gradientMonochrome="info" className="w-24 mx-auto"> Withdraw</Button>
+                    <Button gradientMonochrome="info" className="w-24 mx-auto"> Withdraw</Button>
                 </div>
 
 
@@ -73,7 +73,7 @@ const Deposit: React.FC<Props> = () => {
                     <div className="flex flex-row gap-3 mx-auto">
                     
                         <ApproveButton tokenAddress="0xda9d4f9b69ac6C22e444eD9aF0CfC043b7a7f53f" tokenAmount={value}/>
-                        <Button onClick={() => supplyUSDC()} gradientMonochrome="info" className="w-24"> Supply</Button>
+                        <Button  gradientMonochrome="info" className="w-24"> Supply</Button>
                     </div>
 
                     <div className="text-base font-bold">
@@ -83,7 +83,7 @@ const Deposit: React.FC<Props> = () => {
                         type="text"
                         sizing="md"
                         className="w-full" />
-                    <Button onClick={() => withdrawUSDC()} gradientMonochrome="info" className="w-24 mx-auto"> Withdraw</Button>
+                    <Button  gradientMonochrome="info" className="w-24 mx-auto"> Withdraw</Button>
                 </div>
 
 
